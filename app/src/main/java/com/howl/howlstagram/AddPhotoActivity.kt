2b98bc4616e93e2b909c1b82be8ca5a9914cea1f
Myeongsic <you6878@icloud.com>
2018-08-15 20:09:23 +0900

    8월 14일(5차 날짜 잘못 입력) - SNS앱 만들기 6일차

---
+++ app/src/main/java/com/howl/howlstagram/AddPhotoActivity.kt
@@ -0,0 +1,62 @@
+package com.howl.howlstagram
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+    val PICK_IMAGE_FROM_ALBUM =0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        storage = FirebaseStorage.getInstance()
+
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)
+
+        addphoto_image.setOnClickListener {
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)
+        }
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(resultCode == Activity.RESULT_OK){
+                photoUri = data?.data
+                addphoto_image.setImageURI(data?.data)
+            }
+            else{
+                finish()
+            }
+        }
+
+    }
+    fun contentUpload() {
+
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_"+timeStamp +"_.png"
+        val storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
--- app/src/main/java/com/howl/howlstagram/MainActivity.kt
+++ app/src/main/java/com/howl/howlstagram/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.howl.howlstagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,7 +25,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
-
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
@@ -41,5 +48,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 }
